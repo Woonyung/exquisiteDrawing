b467e2ff4e6ab98e52a4db42c322287ca2f7ec1d
Woonyung <woonyungchoi@gmail.com>
2015-02-15 16:00:49 -0500

    little changes

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var express = require('express');
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
+
+var app = express();
 var mongoose = require('mongoose'); // mongodb
 mongoose.connect(process.env.MONGOLAB_URI); // connect to the mongolab database
 
-var app = express();
 
 app.configure(function(){
 	// server port number
